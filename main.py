1f2f5b98ede714cc77f15a4313efca5cf7d728f5
Naresh <phoenixarjun007@gmail.com>
2025-06-18 14:07:33 +0530

    prepare base model completed using VGG16

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from chest_cancer_detection import logger
 from chest_cancer_detection.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from chest_cancer_detection.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -11,3 +13,15 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME = "Prepare Base Model Stage"
+
+try:
+        logger.info(f"******************************")
+        logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+        logger.exception(e)
+        raise e
